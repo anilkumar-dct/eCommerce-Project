aacc1b7c251673ae49105b18ad80a53243aa9569
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-06-23 17:55:50 +0530

    Initial commit - added routing to my application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./body/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./body/home/home').then((c) => c.Home),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./body/login/login').then((c) => c.Login),
+  },
+  {
+    path: 'seller',
+    loadComponent: () => import('./body/seller/seller').then((c) => c.Seller),
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./body/cart/cart').then((c) => c.Cart),
+  },
+];
