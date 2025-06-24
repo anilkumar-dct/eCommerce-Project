c2a615c58a4f76fcf76be7e8d1f5628c016f2ddf
Anil Kumar <anilkumar.dcttechnology@gmail.com>
2025-06-24 18:49:11 +0530

    Updated header

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -8,11 +8,16 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './header.css',
 })
 export class Header implements OnInit {
+  menuType: string = 'default';
   constructor(private router: Router) {}
   ngOnInit(): void {
     this.router.events.subscribe((val: any) => {
       if (val.url) {
-        console.warn(val.url);
+        if (val.url == '/seller/home' || val.url == '/home') {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
       }
     });
   }
